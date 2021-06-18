b36ecd08debc94fc101fbf4679d79f305e0eee05
wanderlusttt <sbharathraju3@gmail.com>
2021-06-18 20:18:04 +0530

    sdm660-common: Preopt SystemUI
    Signed-off-by: wanderlusttt <sbharathraju3@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -323,6 +323,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.0.vendor
 
+# Preopt SystemUI
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
